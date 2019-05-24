e9a14af58561c426704a737b2b14b0b9e0ceec6f
Windwaker02 <woodringg95@gmail.com>
2019-05-24 12:43:36 -0400

    finished ko handling, added some documentation, started messing with sockets

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
 let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
 
 
-
-
-app.listen(3001, () => {
+server.listen(3001, () => {
     console.log('You got this!')
 })
